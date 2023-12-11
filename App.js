66fc1021f40f8ed28dddcbac70156389b26cd988
bowen-anth <bowen.anth@gmail.com>
2023-12-10 18:44:17 -0700

    changed jsx back to js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Header from './components/Header.jsx'
-import Main from './components/Main.jsx'
-import Footer from './components/Footer.jsx'
+import Header from './components/Header.js'
+import Main from './components/Main.js'
+import Footer from './components/Footer.js'
 
 
 export default function App() {
