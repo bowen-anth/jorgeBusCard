b6cfe69f7d5fab70f9a00d344ce78e2686b162d8
bowen-anth <bowen.anth@gmail.com>
2023-12-10 19:22:25 -0700

    changed webpack to * in package.json

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import Header from '/components/Header.js'
 import Main from '/components/Main.js'
 import Footer from '/components/Footer.js'
 
-
 export default function App() {
     return (
         <div>
